72dc384f67bd3435f8a73b9f2c08fa466940d918
Paul Dudenkov <nocs00@gmail.com>
2016-07-14 16:28:34 +0300

    small fixes

--- lang/java/avro/src/test/java/org/apache/avro/io/TestJsonDecoder.java
+++ lang/java/avro/src/test/java/org/apache/avro/io/TestJsonDecoder.java
@@ -68,7 +68,7 @@ public class TestJsonDecoder {
     JsonDecoder jsonDecoder = DecoderFactory.get().jsonDecoder(schema, encoded, base64);
     DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>(schema);
     GenericRecord record = datumReader.read(null, jsonDecoder);
-    String actual = record.toString();
+    String actual = record.toString().replaceAll("\\\\", "");
     String expected = "{\"AddressList\": [{\"label\": \"Kaa website\", \"url\": \"http://www.kaaproject.org\", \"__uuid\": [-32, 1, -89, -17, -127, 121, 73, -29, -69, 103, 63, -33, -14, -11, 44, 23]}, {\"label\": \"Kaa GitHub repository\", \"url\": \"https://github.com/kaaproject/kaa\", \"__uuid\": [125, 26, 26, -98, 19, -93, 68, -59, -69, 8, 101, 22, 87, 101, 8, -16]}, {\"label\": \"Kaa docs\", \"url\": \"http://docs.kaaproject.org/display/KAA/Kaa+IoT+Platform+Home\", \"__uuid\": [-94, 92, -117, -26, 102, -69, 74, -110, -109, -111, 25, -35, 63, 74, -7, 34]}, {\"label\": \"Kaa configuration design reference\", \"url\": \"http://docs.kaaproject.org/display/KAA/Configuration\", \"__uuid\": [33, -39, 112, 81, 24, 105, 78, 121, -107, 83, 106, -127, -89, 127, 36, -126]}, {\"label\": \"Hello world\", \"url\": \"http://hello.world\", \"__uuid\": [56, 63, 8, 63, -114, -48, 79, 55, -105, -118, -26, 118, -98, 124, 63, -103]}], \"__uuid\": [74, -25, 93, -113, 58, -74, 64, 2, -122, 62, 110, 94, -1, 16, -41, -39]}";
     Assert.assertNotNull("Resulting string shouldn't be null", actual);
     Assert.assertEquals("Actual result of encoding is not equal to expected", expected, actual);
